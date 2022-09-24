e4bcf249e14ad4aae6e8bc8646a884884c9319bc
Ahmet Kürşad YILDIZ <ahmetkursatyildiz_53@hotmail.com>
2022-09-24 13:21:49 +0300

    post methodu ile veri cekmek
    formda action ve method olarak postu belirledik middeware olarak app.use
    express.urlencoded ve express.json ekledik yeni router olarak app.post methodu ekledik form içeriklerini düzenledik

--- app.js
+++ app.js
@@ -10,18 +10,20 @@ const app = express();
 app.set('view engine', 'ejs'); // javascipt engine ejs engine dönüştürüyoruz
 
 //middleware
-const mylogger = (req, res, next) => {
-  console.log('Middleware log 1');
-  next();
-};
-const mylogger2 = (req, res, next) => {
-  console.log('Middleware log 2');
-  next(); //next metodu geri cagırılmadıgı zaman işlem devam etmiyor req ve res arasında kalıyor
-};
+// const mylogger = (req, res, next) => {
+//   console.log('Middleware log 1');
+//   next();
+// };
+// const mylogger2 = (req, res, next) => {
+//   console.log('Middleware log 2');
+//   next(); //next metodu geri cagırılmadıgı zaman işlem devam etmiyor req ve res arasında kalıyor
+// };
 
-app.use(mylogger);
-app.use(mylogger2);
+// app.use(mylogger);
+// app.use(mylogger2);
 app.use(express.static('public')); //static dosyaları public klasörüne attık
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // routes
 app.get('/', (req, res) => {
@@ -37,7 +39,11 @@ app.get('/add', (req, res) => {
   // res.sendFile(path.resolve(__dirname, 'public/index.html')); //klasör bulmak için kullandıgımız method
   res.render('add');
 });
-
+app.post('/photos', (req, res) => {
+  console.log(req.body);
+  res.redirect('/'); //anasayfaya dönmesini saglar
+  
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı`);
