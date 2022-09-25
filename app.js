9072e79d2dbd3c08bb1993b1bdc2f5d3240660db
Ahmet Kürşad YILDIZ <ahmetkursatyildiz_53@hotmail.com>
2022-09-25 17:25:07 +0300

    veri tabanı resim yükleme işlemleri
    veritabanına baglanma işlemleri için mongoose kullanıdık fonksiyonların asekron olabilmesi için routerlarımızı async ve await fonksiyonlarını ekledik
    models klasörü oluşturup mongose ta yeni şema oluştrup fotograf için gerekli parametreleri ekleyip mongoose kullandık kısaca  photo.js index .ejs içine javascript fonksiyonlarını çalıştırdık

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const { request } = require('http');
 const { resolve } = require('path');
-
+const Photo = require('./models/Photo.js');
 const path = require('path');
-
 const app = express();
+//veritabanına baglanma
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 //template engine
 app.set('view engine', 'ejs'); // javascipt engine ejs engine dönüştürüyoruz
 
@@ -26,9 +31,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // routes
-app.get('/', (req, res) => {
+app.get('/', async(req, res) => {
+  const photos = await Photo.find({})
   // res.sendFile(path.resolve(__dirname, 'public/index.html')); //klasör bulmak için kullandıgımız method
-  res.render('index');
+  res.render('index' , {
+   photos
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -39,10 +47,9 @@ app.get('/add', (req, res) => {
   // res.sendFile(path.resolve(__dirname, 'public/index.html')); //klasör bulmak için kullandıgımız method
   res.render('add');
 });
-app.post('/photos', (req, res) => {
-  console.log(req.body);
+app.post('/photos', async(req, res) => {
+  await Photo.create(req.body) //express te json objesşnş almak için kullandıgımız method
   res.redirect('/'); //anasayfaya dönmesini saglar
-  
 });
 const port = 3000;
 app.listen(port, () => {
